638d47756e7f87e1104ae429ea84d0384543dab9
thangtd1993 <thangtd1993@gmail.com>
2016-10-01 17:07:48 +0700

    first commit

---
+++ Assets/Scripts/ChangeNumber.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ChangeNumber : MonoBehaviour {
+    public Button numberButton;
+    public int buttonVal;
+    public int maxVal;
+    // Use this for initialization
+    void Start () {
+        buttonVal = 0;
+        Button btnNum = numberButton.GetComponent<Button>();
+        btnNum.onClick.AddListener(TaskOnClick);
+        numberButton.GetComponentInChildren<Text>().text = "";
+    }
+
+    void TaskOnClick()
+    {
+        if(buttonVal < maxVal)
+        {
+            buttonVal = buttonVal + 1;
+        }
+        else
+        {
+            buttonVal = 1;
+        }
+        
+        //Debug.Log("You have clicked the button!");
+        numberButton.GetComponentInChildren<Text>().text = buttonVal.ToString();
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/CheckNumber.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CheckNumber : MonoBehaviour {
+    public Button checkButton;
+    public Button Button1;
+    public Button Button2;
+    public Button Button3;
+    public Button Button4;
+    public Button Button5;
+    public Button Button6;
+    public Button Button7;
+    public Button Button8;
+    public Button Button9;
+
+    public int btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
+
+    // Use this for initialization
+    void Start () {
+        Button btnChk = checkButton.GetComponent<Button>();
+        btnChk.onClick.AddListener(TaskCheck);
+    }
+
+    void TaskCheck()
+    {
+        
+        Debug.Log(Button1.GetComponentInChildren<Text>().text);
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
